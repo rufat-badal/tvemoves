37198fc32ed419b4708da9b9685b6aec4f4c66d8
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2023-09-18 21:24:44 +0200

    Test coordinates map

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,4 +1,5 @@
 from tvemoves_rufbad.grid import SquareEquilateralGrid
+from pytest import approx
 
 
 def test_square_equilateral_grid():
@@ -8,18 +9,82 @@ def test_square_equilateral_grid():
     num_triangles = 2 * (n - 1) * (n - 1)
     num_boundary_vertices = 4 * (n - 1)
     num_boundary_edges = 4 * (n - 1)
+    num_boundary_vertices_per_side = n
+    num_boundary_edges_per_side = n - 1
 
     G = SquareEquilateralGrid(n)
     assert G.vertices == list(range(num_vertices))
+    assert all(
+        0 <= G.coordinates(v)[0] <= 1 and 0 <= G.coordinates(v)[1] <= 1
+        for v in G.vertices
+    )
     assert len(G.edges) == num_edges
     assert len(G.edges) == len(set(G.edges))
     assert all(v in G.vertices and w in G.vertices for (v, w) in G.edges)
+    assert all(
+        (
+            G.coordinates(v)[0] == approx(G.coordinates(w)[0])
+            and abs(G.coordinates(v)[1] - G.coordinates(w)[1]) == approx(G.grid_spacing)
+        )
+        or (
+            abs(G.coordinates(v)[0] - G.coordinates(w)[0]) == approx(G.grid_spacing)
+            and G.coordinates(v)[1] == approx(G.coordinates(w)[1])
+        )
+        or (
+            abs(G.coordinates(v)[0] - G.coordinates(w)[0]) == approx(G.grid_spacing)
+            and abs(G.coordinates(v)[1] - G.coordinates(w)[1]) == approx(G.grid_spacing)
+        )
+        for (v, w) in G.edges
+    )
     assert len(G.triangles) == num_triangles
     assert len(G.triangles) == len(set(G.triangles))
     assert len(G.boundary_vertices) == num_boundary_vertices
+    assert all(
+        G.coordinates(v)[0] == approx(0)
+        or G.coordinates(v)[0] == approx(1)
+        or G.coordinates(v)[1] == approx(0)
+        or G.coordinates(v)[1] == approx(1)
+        for v in G.boundary_vertices
+    )
     assert len(G.boundary_edges) == num_boundary_edges
     assert all(
         v in G.boundary_vertices and w in G.boundary_vertices
         for (v, w) in G.boundary_edges
     )
+    assert all(
+        (
+            G.coordinates(v)[0] == approx(G.coordinates(w)[0])
+            and abs(G.coordinates(v)[1] - G.coordinates(w)[1]) == approx(G.grid_spacing)
+        )
+        or (
+            abs(G.coordinates(v)[0] - G.coordinates(w)[0]) == approx(G.grid_spacing)
+            and G.coordinates(v)[1] == approx(G.coordinates(w)[1])
+        )
+        for (v, w) in G.boundary_edges
+    )
     assert all(e in G.edges for e in G.boundary_edges)
+    assert G.neumann_vertices == G.boundary_vertices
+    assert G.neumann_edges == G.boundary_edges
+    assert G.dirichlet_vertices == []
+    assert G.dirichlet_edges == []
+
+    G_lower_fixed = SquareEquilateralGrid(n, fix="lower")
+    assert all(
+        G_lower_fixed.coordinates(v)[1] == approx(0)
+        for v in G_lower_fixed.dirichlet_vertices
+    )
+    G_right_fixed = SquareEquilateralGrid(n, fix="right")
+    assert all(
+        G_right_fixed.coordinates(v)[0] == approx(1)
+        for v in G_right_fixed.dirichlet_vertices
+    )
+    G_upper_fixed = SquareEquilateralGrid(n, fix="upper")
+    assert all(
+        G_upper_fixed.coordinates(v)[1] == approx(1)
+        for v in G_upper_fixed.dirichlet_vertices
+    )
+    G_left_fixed = SquareEquilateralGrid(n, fix="left")
+    assert all(
+        G_left_fixed.coordinates(v)[0] == approx(0)
+        for v in G_left_fixed.dirichlet_vertices
+    )
