ed4bfda2b1d7142b42ac2ad034165f418c5df46d
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2023-09-23 19:40:01 +0200

    Tested integrator

--- src/tvemoves_rufbad/integrators.py
+++ src/tvemoves_rufbad/integrators.py
@@ -10,6 +10,21 @@ class Integrator:
             triangle_points[k] - triangle_points[i] for (i, _, k) in self._triangles
         )
         self._triangle_areas = [
-            first.vstack(second).det() / 2
+            abs(first.vstack(second).det() / 2)
             for (first, second) in zip(first_sides, second_sides)
         ]
+
+    def __call__(self, integrand):
+        # integrand(triangle, (b1, b2)) should return a float,
+        # where triangle is any triangle in self._triangles
+        # and (b1, b2, 1 - b1 - b2) are barycentric coordinates
+        return sum(
+            triangle_area
+            * sum(
+                weight * integrand(triangle, point)
+                for (point, weight) in zip(
+                    self._quadrature.points, self._quadrature.weights
+                )
+            )
+            for (triangle_area, triangle) in zip(self._triangle_areas, self._triangles)
+        )
--- tests/test_integrators.py
+++ tests/test_integrators.py
@@ -1,6 +1,79 @@
 from tvemoves_rufbad.integrators import Integrator
 from tvemoves_rufbad.grid import SquareEquilateralGrid
-from tvemoves_rufbad.quadrature_rules import CENTROID
+from tvemoves_rufbad.quadrature_rules import (
+    CENTROID,
+    VERTEX,
+    DUNAVANT2,
+    DUNAVANT3,
+    DUNAVANT4,
+    DUNAVANT5,
+)
+from math import sin, cos, pi
 
-grid = SquareEquilateralGrid(num_horizontal_points=10)
-integral = Integrator(CENTROID, grid)
+TRIANGLE_QUADRATURE_RULES = [
+    CENTROID,
+    VERTEX,
+    DUNAVANT2,
+    DUNAVANT3,
+    DUNAVANT4,
+    DUNAVANT5,
+]
+
+GAUSS_DEGREES = [1, 2, 3, 4, 5, 5, 7]
+
+
+def constant(x, y):
+    return 1
+
+
+INT_CONSTANT = 1
+
+
+def parabola(x, y):
+    return x**2 + y**2
+
+
+INT_PARABOLA = 2 / 3
+
+
+def periodic(x, y):
+    return sin(2 * pi * x) * cos(2 * pi * y)
+
+
+INT_PERIODIC = 0
+
+
+def generate_integrand(f, grid_points):
+    def f_integrand(triangle, barycentric_coordinates):
+        i1, i2, i3 = triangle
+        b1, b2 = barycentric_coordinates
+        b3 = 1 - b1 - b2
+        p = b1 * grid_points[i1] + b2 * grid_points[i2] + b3 * grid_points[i3]
+        return f(*p)
+
+    return f_integrand
+
+
+functions = [constant, parabola, periodic]
+integral_values = [INT_CONSTANT, INT_PARABOLA, INT_PERIODIC]
+
+EPS = 1e-5
+
+
+def test_integrator():
+    max_horizontal_points = 256
+    for quadrature in TRIANGLE_QUADRATURE_RULES:
+        for f, integral_value in zip(functions, integral_values):
+            num_horizontal_points = 2
+            approximation_converges = False
+            while num_horizontal_points <= max_horizontal_points:
+                grid = SquareEquilateralGrid(num_horizontal_points)
+                integrand = generate_integrand(f, grid.initial_positions)
+                integrator = Integrator(quadrature, grid)
+                error = abs(integrator(integrand) - integral_value)
+                if error < EPS:
+                    approximation_converges = True
+                    break
+                num_horizontal_points *= 2
+            print(error)
+            assert approximation_converges
