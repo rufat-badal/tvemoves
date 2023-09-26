9b8381470d84ba821226357cbc46137050c868ae
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2023-09-26 19:44:17 +0200

    Save first step

--- src/tvemoves_rufbad/simulation.py
+++ src/tvemoves_rufbad/simulation.py
@@ -2,6 +2,18 @@ import pyomo.environ as pyo
 from .mechanical_step import MechanicalStep
 
 
+class Step:
+    def __init__(self, y, theta):
+        self.y = y
+        self.theta = theta
+
+    def __repr__(self):
+        return f"Step(y={repr(self.y)}, theta={repr(self.theta)})"
+
+    def __str__(self):
+        return f"y:\n{str(self.y)}\ntheta:\n{str(self.theta)}"
+
+
 class Simulation:
     def __init__(
         self, grid, initial_temperature, search_radius, shape_memory_scaling, fps
@@ -16,3 +28,6 @@ class Simulation:
             fps,
         )
         self._mechanical_step.solve()
+        self.steps = [
+            Step(self._mechanical_step.prev_y(), self._mechanical_step.prev_theta())
+        ]
