cd2d79089407b196dc69dc09fd9b4beac1fecba6
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2023-09-29 09:56:28 +0200

    Started work on testing c1 interpolation

--- tests/test_interpolation.py
+++ tests/test_interpolation.py
@@ -1,4 +1,8 @@
-from tvemoves_rufbad.interpolation import P1Interpolation, P1Deformation
+from tvemoves_rufbad.interpolation import (
+    P1Interpolation,
+    P1Deformation,
+    C1Interpolation,
+)
 from tvemoves_rufbad.grid import generate_square_equilateral_grid
 from tvemoves_rufbad.tensors import Vector, Matrix
 import pyomo.environ as pyo
@@ -14,12 +18,20 @@ def gradient_affine(x, y):
     return Vector([1, 1])
 
 
+def hessian_affine(x, y):
+    return Matrix([[0, 0], [0, 0]])
+
+
 def parabola(x, y):
     return (x - 1 / 2) ** 2 + (y - 1 / 2) ** 2
 
 
 def gradient_parabola(x, y):
     return Vector([2 * (x - 1 / 2), 2 * (y - 1 / 2)])
+
+
+def hessian_parabola(x, y):
+    return Matrix([[2, 0], [0, 2]])
 
 
 def periodic(x, y):
@@ -35,8 +47,28 @@ def gradient_periodic(x, y):
     )
 
 
+def hessian_periodic(x, y):
+    return (
+        -4
+        * pi**2
+        * Matrix(
+            [
+                [
+                    pyo.cos(2 * pi * y) * pyo.sin(2 * pi * x),
+                    pyo.sin(2 * pi * y) * pyo.cos(2 * pi * x),
+                ],
+                [
+                    pyo.cos(2 * pi * x) * pyo.sin(2 * pi * y),
+                    pyo.sin(2 * pi * x) * pyo.cos(2 * pi * y),
+                ],
+            ]
+        )
+    )
+
+
 functions = [affine, parabola, periodic]
 gradients = [gradient_affine, gradient_parabola, gradient_periodic]
+hessians = [hessian_affine, hessian_parabola, hessian_periodic]
 
 
 def affine_deformation(x, y):
@@ -178,3 +210,99 @@ def test_p1_deformation():
             )
         ) / len(grid.triangles)
         assert mean_squared_strain_error < grad_eps
+
+
+def test_p1_interpolation_with_pyomo_params():
+    grid = generate_square_equilateral_grid(num_horizontal_points=50)
+
+    for f in functions:
+        params = [f(*p) for p in grid.points]
+        f_approx = P1Interpolation(grid, params)
+        values = [f_approx(triangle, (1 / 3, 1 / 3)) for triangle in grid.triangles]
+
+        model = pyo.ConcreteModel()
+        model.params = pyo.Param(
+            grid.vertices,
+            within=pyo.Reals,
+            initialize=params,
+            mutable=True,
+        )
+        f_approx_pyomo = P1Interpolation(grid, model.params)
+        values_pyomo = [
+            pyo.value(f_approx_pyomo(triangle, (1 / 3, 1 / 3)))
+            for triangle in grid.triangles
+        ]
+        assert values == approx(values_pyomo)
+
+        grad_values = [f_approx.gradient(triangle)._data for triangle in grid.triangles]
+        grad_values_pyomo = [
+            f_approx_pyomo.gradient(triangle).map(pyo.value)._data
+            for triangle in grid.triangles
+        ]
+        assert grad_values == grad_values_pyomo
+
+
+def test_p1_deformation():
+    eps = 1e-6
+    grad_eps = 1e-3
+    grid = generate_square_equilateral_grid(num_horizontal_points=100)
+    p0 = grid.points
+    evaluation_points = [
+        p0[i1] / 3 + p0[i2] / 3 + p0[i3] / 3 for (i1, i2, i3) in grid.triangles
+    ]
+
+    for deform, strain in zip(deformations, strains):
+        params_vectors = [deform(*p) for p in grid.points]
+        params = (
+            [v[0] for v in params_vectors],
+            [v[1] for v in params_vectors],
+        )
+        deform_approx = P1Deformation(grid, *params)
+
+        values = [deform(*p) for p in evaluation_points]
+        values_approx = [
+            deform_approx(triangle, (1 / 3, 1 / 3)).map(pyo.value)
+            for triangle in grid.triangles
+        ]
+        mean_squared_error = sum(
+            (value - value_approx).normsqr()
+            for (value, value_approx) in zip(values, values_approx)
+        ) / len(grid.triangles)
+        assert mean_squared_error < eps
+
+        strain_values = [strain(*p) for p in evaluation_points]
+        strain_values_approx = [
+            deform_approx.strain(triangle) for triangle in grid.triangles
+        ]
+        mean_squared_strain_error = sum(
+            (strain_value - strain_value_approx).normsqr()
+            for (strain_value, strain_value_approx) in zip(
+                strain_values, strain_values_approx
+            )
+        ) / len(grid.triangles)
+        assert mean_squared_strain_error < grad_eps
+
+
+def test_c1_interpolation():
+    eps = 1e-6
+    grad_eps = 1e-2
+    grid = generate_square_equilateral_grid(num_horizontal_points=2)
+    p0 = grid.points
+    evaluation_points = [
+        p0[i1] / 3 + p0[i2] / 3 + p0[i3] / 3 for (i1, i2, i3) in grid.triangles
+    ]
+
+    for f, grad_f, hessian_f in zip(functions, gradients, hessians):
+        f_at_grid_points = [f(*p) for p in grid.points]
+        grad_f_at_grid_points = [grad_f(*p) for p in grid.points]
+        hessian_f_at_grid_points = [hessian_f(*p) for p in grid.points]
+        params = [
+            [f, G[0], G[1], H[0, 0], H[0, 1], H[1, 1]]
+            for (f, G, H) in zip(
+                f_at_grid_points, grad_f_at_grid_points, hessian_f_at_grid_points
+            )
+        ]
+        print(params[0])
+
+
+test_c1_interpolation()
