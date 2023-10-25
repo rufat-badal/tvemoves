ba12b1e8d55c87a2ef7ac9cd49406d13e8b36adc
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2023-10-25 12:32:20 +0200

    Added docstrings

--- src/tvemoves_rufbad/interpolation.py
+++ src/tvemoves_rufbad/interpolation.py
@@ -1,14 +1,18 @@
+"""Module providing interpolations in triangles."""
+
+from typing import Iterable
 from tvemoves_rufbad.tensors import Vector, Matrix, Tensor3D
 from tvemoves_rufbad.grid import Grid, Triangle, BarycentricCoordinates, Edge
-import numpy.typing as npt
 
 
 class P1Interpolation:
-    def __init__(self, grid: Grid, params: list):
+    """Piecewise affine interpolation."""
+
+    def __init__(self, grid: Grid, params: Iterable):
         if len(params) != len(grid.vertices):
             raise ValueError("number of params must equal to the number of vertices")
         self._grid = grid
-        self._params = params
+        self._params = list(params)
 
     def __call__(
         self,
@@ -20,16 +24,23 @@ class P1Interpolation:
         return t1 * self._params[i1] + t2 * self._params[i2] + t3 * self._params[i3]
 
     def on_boundary(self, edge: Edge, t: float):
+        """Computes the interpolation on an edge."""
         i1, i2 = edge
         return t * self._params[i1] + (1 - t) * self._params[i2]
 
     def gradient(self, triangle: Triangle, barycentric_coordinates=None) -> Vector:
+        """Computes gradient of the interpolation."""
+        # Unused argument 'barycentric_coordinates' assures the same calling convention
+        # as in the case of the C1 interpolation
+        del barycentric_coordinates
         i1, i2, i3 = triangle
         area_gradient = Vector([self._params[i1], self._params[i2], self._params[i3]])
         return self._grid.gradient_transform(triangle, area_gradient)
 
 
 class P1Deformation:
+    """Deformation computed via piecewise affine interpolation."""
+
     def __init__(self, grid, y1_params: list, y2_params: list):
         self._y = [P1Interpolation(grid, y1_params), P1Interpolation(grid, y2_params)]
 
@@ -45,7 +56,8 @@ class P1Deformation:
             ]
         )
 
-    def on_boundary(self, edge: Edge, t: float):
+    def on_edge(self, edge: Edge, t: float):
+        """Computes deformation on an edge."""
         return Vector(
             [
                 self._y[0].on_boundary(edge, t),
@@ -54,6 +66,8 @@ class P1Deformation:
         )
 
     def strain(self, triangle: Triangle, barycentric_coordinates=None) -> Matrix:
+        """Computes strain of the deformation."""
+        del barycentric_coordinates
         return self._y[0].gradient(triangle).stack(self._y[1].gradient(triangle))
 
     def __getitem__(self, i: int):
@@ -61,6 +75,8 @@ class P1Deformation:
 
 
 class C1Interpolation:
+    """Interpolation using degree 5 polynomials assuring C1 regularity."""
+
     def __init__(self, grid: Grid, params: list[list]):
         self._grid = grid
         # each param is a list of 6 floats corresponding to
@@ -74,29 +90,32 @@ class C1Interpolation:
         barycentric_coordinates: BarycentricCoordinates,
     ):
         i1, i2, i3 = triangle
-        L1, L2, L3 = barycentric_coordinates
-        Ns = [
-            self._grid.shape_function((i1, i2, i3), (L1, L2, L3)),
-            self._grid.shape_function((i2, i3, i1), (L2, L3, L1)),
-            self._grid.shape_function((i3, i1, i2), (L3, L1, L2)),
+        l1, l2, l3 = barycentric_coordinates
+        shape_function_values = [
+            self._grid.shape_function((i1, i2, i3), (l1, l2, l3)),
+            self._grid.shape_function((i2, i3, i1), (l2, l3, l1)),
+            self._grid.shape_function((i3, i1, i2), (l3, l1, l2)),
         ]
-        ps = [self._params[i1], self._params[i2], self._params[i3]]
-        return sum(N.dot(p) for (N, p) in zip(Ns, ps))
+        params = [self._params[i1], self._params[i2], self._params[i3]]
+        return sum(
+            shape_function_value.dot(param)
+            for (shape_function_value, param) in zip(shape_function_values, params)
+        )
 
     def _area_gradient(
         self, triangle: Triangle, barycentric_coordinates: BarycentricCoordinates
     ) -> Vector:
         i1, i2, i3 = triangle
-        L1, L2, L3 = barycentric_coordinates
+        l1, l2, l3 = barycentric_coordinates
 
         gradient1 = (
-            self._grid.shape_function_jacobian((i1, i2, i3), (L1, L2, L3))
+            self._grid.shape_function_jacobian((i1, i2, i3), (l1, l2, l3))
             .transpose()
             .dot(self._params[i1])
         )
         gradient2 = (
             (
-                self._grid.shape_function_jacobian((i2, i3, i1), (L2, L3, L1))
+                self._grid.shape_function_jacobian((i2, i3, i1), (l2, l3, l1))
                 @ Matrix([[0, 1, 0], [0, 0, 1], [1, 0, 0]])
             )
             .transpose()
@@ -104,7 +123,7 @@ class C1Interpolation:
         )
         gradient3 = (
             (
-                self._grid.shape_function_jacobian((i3, i1, i2), (L3, L1, L2))
+                self._grid.shape_function_jacobian((i3, i1, i2), (l3, l1, l2))
                 @ Matrix([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
             )
             .transpose()
@@ -115,6 +134,7 @@ class C1Interpolation:
     def gradient(
         self, triangle: Triangle, barycentric_coordinates: BarycentricCoordinates
     ) -> Vector:
+        """Computes gradient of the interpolation."""
         return self._grid.gradient_transform(
             triangle, self._area_gradient(triangle, barycentric_coordinates)
         )
@@ -123,16 +143,16 @@ class C1Interpolation:
         self, triangle: Triangle, barycentric_coordinates: BarycentricCoordinates
     ) -> Vector:
         i1, i2, i3 = triangle
-        L1, L2, L3 = barycentric_coordinates
+        l1, l2, l3 = barycentric_coordinates
 
         hessian1 = (
-            self._grid.shape_function_hessian_vectorized((i1, i2, i3), (L1, L2, L3))
+            self._grid.shape_function_hessian_vectorized((i1, i2, i3), (l1, l2, l3))
             .transpose()
             .dot(self._params[i1])
         )
         hessian2 = (
             (
-                self._grid.shape_function_hessian_vectorized((i2, i3, i1), (L2, L3, L1))
+                self._grid.shape_function_hessian_vectorized((i2, i3, i1), (l2, l3, l1))
                 @ Matrix(
                     [
                         [0, 1, 0, 0, 0, 0],
@@ -149,7 +169,7 @@ class C1Interpolation:
         )
         hessian3 = (
             (
-                self._grid.shape_function_hessian_vectorized((i3, i1, i2), (L3, L1, L2))
+                self._grid.shape_function_hessian_vectorized((i3, i1, i2), (l3, l1, l2))
                 @ Matrix(
                     [
                         [0, 0, 1, 0, 0, 0],
@@ -169,12 +189,14 @@ class C1Interpolation:
     def hessian(
         self, triangle: Triangle, barycentric_coordinates: BarycentricCoordinates
     ) -> Matrix:
+        """Computes hessian of the interpolation."""
         return self._grid.hessian_transform(
             triangle,
             self._area_hessian_vectorized(triangle, barycentric_coordinates),
         )
 
     def on_edge(self, edge: Edge, t: float):
+        """Computes the interpolation on an edge."""
         i, j = edge
         p, q = self._params[i], self._params[j]
         return self._grid.shape_function_on_edge_left(edge, t).dot(
@@ -183,6 +205,8 @@ class C1Interpolation:
 
 
 class C1Deformation:
+    """Deformation computed via C1 interpolation."""
+
     def __init__(self, grid, y1_params: list[list], y2_params: list[list]):
         self._y = [C1Interpolation(grid, y1_params), C1Interpolation(grid, y2_params)]
 
@@ -201,6 +225,7 @@ class C1Deformation:
     def strain(
         self, triangle: Triangle, barycentric_coordinates: BarycentricCoordinates
     ) -> Matrix:
+        """Computes strain of the deformation."""
         return (
             self._y[0]
             .gradient(triangle, barycentric_coordinates)
@@ -210,9 +235,11 @@ class C1Deformation:
     def hyper_strain(
         self, triangle: Triangle, barycentric_coordinates: BarycentricCoordinates
     ) -> Tensor3D:
+        """Computes the hyper strain of the deformation."""
         hessian_y1 = self._y[0].hessian(triangle, barycentric_coordinates)
         hessian_y2 = self._y[1].hessian(triangle, barycentric_coordinates)
         return hessian_y1.stack(hessian_y2)
 
     def on_edge(self, edge: Edge, t: float) -> Vector:
+        """Computes deformation on an edge."""
         return Vector([self._y[0].on_edge(edge, t), self._y[1].on_edge(edge, t)])
