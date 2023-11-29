9f6ba33c2f7a756a43963fa8e788b56cf457a35f
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2023-11-29 14:42:13 +0100

    Tested 1D Gauss quadrature

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,7 +2,7 @@ import sympy as sp
 import random
 
 
-def random_symbolic_polynomial(
+def random_symbolic_polynomial_2d(
     degree: int, x: sp.Symbol, y: sp.Symbol
 ) -> sp.core.expr.Expr:
     if degree < 0:
@@ -16,5 +16,18 @@ def random_symbolic_polynomial(
     for i, cs in enumerate(coefficients):
         for j, c in enumerate(cs):
             poly += c * x ** (i - j) * y**j
+
+    return poly
+
+
+def random_symbolic_polynomial_1d(degree: int, x: sp.Symbol) -> sp.core.expr.Expr:
+    if degree < 0:
+        raise ValueError("degree must be non-negative")
+
+    coefficients = [random.uniform(-10.0, 10.0) for _ in range(degree + 1)]
+
+    poly = 0
+    for i, c in enumerate(coefficients):
+        poly += c * x**i
 
     return poly
--- tests/test_quadrature_rules.py
+++ tests/test_quadrature_rules.py
@@ -1,8 +1,12 @@
-""""Test quadrature rules and integrators."""
+"""Test quadrature rules."""
+
 
 import sympy as sp
-from .helpers import random_symbolic_polynomial
-from tvemoves_rufbad.quadrature_rules import DUNAVANT_QUADRATURE_RULES
+from tvemoves_rufbad.quadrature_rules import (
+    DUNAVANT_QUADRATURE_RULES,
+    GaussQuadratureRule,
+)
+from .helpers import random_symbolic_polynomial_2d, random_symbolic_polynomial_1d
 
 
 def test_dunavant() -> None:
@@ -14,9 +18,27 @@ def test_dunavant() -> None:
     eps = 1e-14
 
     for i, rule in enumerate(DUNAVANT_QUADRATURE_RULES):
+        # rule i must integrate a polynomial of degree i + 1 exactly
         degree = i + 1
-        poly_symbolic = random_symbolic_polynomial(degree, x, y)
+        poly_symbolic = random_symbolic_polynomial_2d(degree, x, y)
         poly_integral = sp.integrate(poly_symbolic, (y, 0, 1 - x), (x, 0, 1))
         poly = sp.lambdify([x, y], poly_symbolic)
         poly_quadrature = 0.5 * sum(w * poly(p[1], p[2]) for w, p in rule)
         assert abs(poly_quadrature - poly_integral) < eps
+
+
+def test_gauss() -> None:
+    """Test 1D Gauss quadrature. A quadrature rule of degree d should exactly integrate
+    polynomials of degree d.
+    """
+
+    x = sp.Symbol("x")
+    eps = 1e-14
+
+    for degree in range(1, 6):
+        rule = GaussQuadratureRule(degree)
+        poly_symbolic = random_symbolic_polynomial_1d(degree, x)
+        poly_integral = sp.integrate(poly_symbolic, (x, 0, 1))
+        poly = sp.lambdify([x], poly_symbolic)
+        poly_quadrature = sum(w * poly(p) for w, p in rule)
+        assert abs(poly_integral - poly_quadrature) < eps
