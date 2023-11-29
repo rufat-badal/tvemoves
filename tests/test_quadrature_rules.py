1a71ece2075fd7386b76893ed3b4df54ffc7baea
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2023-11-29 14:23:19 +0100

    Fixed wrong points and weights

--- tests/test_quadrature_rules.py
+++ tests/test_quadrature_rules.py
@@ -1,7 +1,7 @@
 """"Test quadrature rules and integrators."""
 
 import sympy as sp
-from helpers import random_symbolic_polynomial
+from .helpers import random_symbolic_polynomial
 from tvemoves_rufbad.quadrature_rules import DUNAVANT_QUADRATURE_RULES
 
 
@@ -11,20 +11,12 @@ def test_dunavant() -> None:
     """
 
     x, y = sp.symbols("x y")
+    eps = 1e-14
 
-    rule = DUNAVANT_QUADRATURE_RULES[1]
-    for degree in range(6):
+    for i, rule in enumerate(DUNAVANT_QUADRATURE_RULES):
+        degree = i + 1
         poly_symbolic = random_symbolic_polynomial(degree, x, y)
         poly_integral = sp.integrate(poly_symbolic, (y, 0, 1 - x), (x, 0, 1))
-        print(poly_integral)
-
         poly = sp.lambdify([x, y], poly_symbolic)
-        poly_quadrature = 0.5 * sum(
-            w * poly(p[1], p[2]) for (w, p) in zip(rule.weights, rule.points)
-        )
-        print(poly_quadrature)
-
-        print()
-
-
-test_dunavant()
+        poly_quadrature = 0.5 * sum(w * poly(p[1], p[2]) for w, p in rule)
+        assert abs(poly_quadrature - poly_integral) < eps
