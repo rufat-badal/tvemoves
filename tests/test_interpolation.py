3d1d91dfcbcd2420f02a71d24562b8b77368c24c
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2023-12-08 15:46:37 +0100

    Refactored grid point generation

--- tests/test_interpolation.py
+++ tests/test_interpolation.py
@@ -1,7 +1,13 @@
 """Test all interpolations"""
 
 from tvemoves_rufbad.interpolation import P1Interpolation
-from tvemoves_rufbad.domain import RectangleDomain
+from tvemoves_rufbad.domain import (
+    RectangleDomain,
+    BarycentricCoordinates,
+    Triangle,
+    Grid,
+)
+from tvemoves_rufbad.tensors import Vector
 from .helpers import random_polynomial_2d, random_barycentric_coordinates
 
 GRID_WIDTH = 3
@@ -10,23 +16,28 @@ GRID_SCALE = 0.5
 EPS = 1e-13
 
 
+def _random_test_points(
+    grid: Grid,
+    points_per_triangle: int = 1,
+) -> list[tuple[tuple[Triangle, BarycentricCoordinates], Vector]]:
+    test_points = []
+    for triangle in grid.triangles:
+        p1, p2, p3 = grid.triangle_vertices(triangle)
+        for bc in random_barycentric_coordinates(points_per_triangle):
+            test_points.append(((triangle, bc), bc.u * p1 + bc.v * p2 + bc.w * p3))
+
+    return test_points
+
+
 def test_p1_interpolation() -> None:
     """Test piecewise affine interpolation"""
-    poly, poly_gradient = random_polynomial_2d(1, 1)
+    poly, poly_gradient = random_polynomial_2d(degree=1, num_derivatives=1)
     rect = RectangleDomain(GRID_WIDTH, GRID_HEIGHT)
     grid = rect.grid(GRID_SCALE)
 
     params = [poly(*p) for p in grid.points]
     interpolation = P1Interpolation(grid, params)
     # One barycentric coordinate per triangle
-    barycentric_coordinates = random_barycentric_coordinates(len(grid.triangles))
-    euclidean_coordinates = []
-    for bc, triangle in zip(barycentric_coordinates, grid.triangles):
-        p1, p2, p3 = grid.triangle_vertices(triangle)
-        euclidean_coordinates.append(bc.u * p1 + bc.v * p2 + bc.w * p3)
-
-    for bc, triangle, ec in zip(
-        barycentric_coordinates, grid.triangles, euclidean_coordinates
-    ):
+    for (triangle, bc), ec in _random_test_points(grid):
         assert abs(poly(*ec) - interpolation(triangle, bc)) < EPS
         assert (poly_gradient(*ec) - interpolation.gradient(triangle)).norm() < EPS
